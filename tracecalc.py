d24c124f2478104f5a2e644fa264d27b3cd1a246
Schar21 <104032727+Schar21@users.noreply.github.com>
2024-03-26 22:19:52 +0100

    Update tracecalc.py

--- tracecalc.py
+++ tracecalc.py
@@ -9,29 +9,60 @@ class trace:
 
 
 def diff():
-    differ = max_trace.base_amt() - current_trace.base_amt()
-    per_run = trace(1.25, 1, 0.158)
-    erun = per_run.base_amt()
-    runs = int(differ / erun)
-    days = int(runs/max_runs_daily)
-    print(f" The maximum amount of runs to max out a character is {int(max_trace.base_amt()/erun)}, or around"
-          f" {int(max_trace.base_amt()/(erun*max_runs_daily))} days.")
-    if differ == 0:
-        return " The total amount of trace materials is sufficient. "
-    elif differ < 0:
-        return f" You have exceeded the required amount of runs by {-runs}, and have farmed for {-days} days more. "
-    elif differ > 0:
-        return (f" You need to farm for an additional {days} day(s),"
-                f" and run an average of {runs} more runs to have sufficient amount of traces. ")
+    per_run_hsr = trace(1.25, 1, 0.158)
+    per_run_gen = trace(2.2, 1.97, 0.24)
+    game = input(" For which game do you want to calculate for? (H/G): ")
+    if game.upper() == "H":
+        differ = max_trace_hsr.base_amt() - current_trace.base_amt()
+        erun = per_run_hsr.base_amt()
+        runs = int(differ / erun)
+        days = int(runs / max_runs_daily_hsr)
+        if differ == 0:
+            return " The total amount of trace materials is sufficient. "
+        elif differ < 0:
+            return f" You have exceeded the required amount of runs by {-runs}, and have farmed for {-days} days more. "
+        elif differ > 0:
+            print(f" The maximum amount of runs to max out a character is {int(max_trace_hsr.base_amt() / erun)},"
+                  f" or around {int(max_trace_hsr.base_amt() / (erun * max_runs_daily_hsr))} days.")
+            return (f" You need to farm for an additional {days} day(s),"
+                    f" and run an average of {runs} more runs to have sufficient amount of traces. ")
+    elif game.upper() == "G":
+        differ = max_trace_gen.base_amt() - current_trace.base_amt()
+        erun = per_run_gen.base_amt()
+        runs = int(differ / erun)
+        days = int(runs / max_runs_daily_gen)
+        if differ == 0:
+            return " The total amount of talent books is sufficient. "
+        elif differ < 0:
+            return f" You have exceeded the required amount of runs by {-runs}, and have farmed for {-days} days more. "
+        elif differ > 0:
+            print(f" For a single crown you need {int(max_trace_gen.base_amt() / (erun * 3))} runs.")
+            print(f" For a double crown you need {int((max_trace_gen.base_amt() * 2) / (erun * 3))} runs.")
+            print(f" For a triple crown you need {int(max_trace_gen.base_amt() / erun)} runs.")
+            print(
+                f" The maximum amount of runs to single crown a character is {int(max_trace_gen.base_amt() / (3 * erun))},"
+                f" or around"
+                f" {int(max_trace_gen.base_amt() / (3 * erun * max_runs_daily_gen))} days.")
+            print(
+                f" The maximum amount of runs to double crown a character is {int((max_trace_gen.base_amt() * 2) / (3 * erun))},"
+                f" or around"
+                f" {int((2 * max_trace_gen.base_amt()) / (3 * erun * max_runs_daily_gen))} days.")
+            print(f" The maximum amount of runs to triple crown a character is {int(max_trace_gen.base_amt() / erun)},"
+                  f" or around"
+                  f" {int(max_trace_gen.base_amt() / (erun * max_runs_daily_gen))} days.")
+            return (f" You need to farm for an additional {days} day(s),"
+                    f" and run an average of {runs} more runs to have sufficient amount of talent books. ")
 
 
-max_trace = trace(18, 69, 139)
-print(f" The maximum trace materials required to max out a character is {max_trace.base_amt()}")
-max_power = 240
-max_runs_daily = 240/10
-c1 = int(input(" Please enter trace materials of rarity 1: "))
-c2 = int(input(" Please enter trace materials of rarity 2: "))
-c3 = int(input(" Please enter trace materials of rarity 3: "))
+max_trace_hsr = trace(18, 69, 139)
+max_trace_gen = trace(9, 114, 63)
+max_power_hsr = 240
+max_runs_daily_hsr = 24
+max_power_gen = 160
+max_runs_daily_gen = 8
+c1 = int(input(" Please enter trace materials/books of rarity 1: "))
+c2 = int(input(" Please enter trace materials/books of rarity 2: "))
+c3 = int(input(" Please enter trace materials/books of rarity 3: "))
 
 current_trace = trace(c1, c2, c3)
 
